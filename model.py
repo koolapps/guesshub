710a2d924459d11f301f335eb30beba25dff6b39
max99x <max99x@gmail.com>
2013-11-02 16:03:20 -0700

    Add a Repository class stub.

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
-SCHEMA = ''  # TODO
+import re
+
+# A regex to pull the repository name from a commit URL.
+RE_REPOSITORY = re.compile(
+    '$https://api.github.com/repos/\w+/\w+/commits/[a-fA-F0-9]+$')
+
 
 class Commit(object):
   """TODO"""
@@ -8,6 +13,10 @@ class Commit(object):
     self.message = commit_json['commit']['message']
     self.author = commit_json['author']['login']
     self.author_avatar_url = commit_json['author']['avatar_url']
+
+    repository_matches = RE_REPOSITORY.findall(commit_json['url'])
+    assert len(repository_matches) == 1, commit_json
+    self.repository = repository_matches[1]
 
     self.contents_url = patch_json['raw_url']
     self.filename = patch_json['filename']
@@ -19,3 +28,16 @@ class Commit(object):
   def from_json(self, json):
     """TODO"""
     return [Commit(json, patch_json) for patch_json in json['files']]
+
+
+class Reository(object):
+  """TODO"""
+
+  def __init__(self, repository_json):
+    self.id = repository_json['id']
+    self.name = repository_json['full_name']
+    self.author = repository_json['owner']['login']
+    self.author_avatar_url = repository_json['owner']['avatar_url']
+    self.description = repository_json['description']
+    self.is_private = repository_json['private']
+    self.is_fork = repository_json['fork']
