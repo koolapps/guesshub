7c8fa8a4db6a3a10f5d5c1d4066e2a7fb4d97939
max99x <max99x@gmail.com>
2013-11-05 13:15:42 -0800

    Fix more edge cases in the crawler script; add docstrings.

--- crawl.py
+++ crawl.py
@@ -10,7 +10,14 @@ PAGES = 1
 BATCH_SIZE = 10
 
 
-def insert_repository(db, repo):
+def encode(row):
+  """UTF8-encodes all strings in an iterable, returning a tuple."""
+  return tuple([i.encode('utf8') if isinstance(i, unicode)
+                else i for i in row])
+
+
+def insert_repository(cursor, repo):
+  """Inserts a Repository object into the database using a given cursor."""
   sql = 'REPLACE INTO repository VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
   row = (repo.id,
          repo.name,
@@ -21,61 +28,86 @@ def insert_repository(db, repo):
          repo.is_fork,
          repo.watcher_count,
          repo.star_count)
-  db.execute(sql, row)
+  cursor.execute(sql, encode(row))
 
 
-def insert_commit(db, commits):
+def insert_commit(cursor, commits):
+  """Inserts a Commit object into the database using a given cursor."""
   sql = ('REPLACE INTO commit VALUES'
          '(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)')
   rows = []
+
+  def flush():
+    try:
+      cursor.executemany(sql, rows)
+    except Exception as e:
+      print commits[0].repository, [commit.sha for commit in commits]
+      raise e
+
   for commit in commits:
-    rows.append((commit.sha,
-                 commit.patch_number,
-                 commit.message.encode('utf8'),
-                 commit.author_login,
-                 commit.author_name and commit.author_name.encode('utf8'),
-                 commit.author_avatar_url,
-                 commit.repository,
-                 commit.file_contents_url,
-                 None,  # file_contents
-                 commit.filename,
-                 commit.additions,
-                 commit.deletions,
-                 commit.old_start_line,
-                 commit.new_start_line,
-                 commit.block_name and commit.block_name.encode('utf8'),
-                 u'\n'.join(commit.diff_lines).encode('utf8')))
+    rows.append(encode((commit.sha,
+                        commit.patch_number,
+                        commit.message,
+                        commit.author_login,
+                        commit.author_name,
+                        commit.author_avatar_url,
+                        commit.repository,
+                        commit.file_contents_url,
+                        None,  # file_contents
+                        commit.filename,
+                        commit.additions,
+                        commit.deletions,
+                        commit.old_start_line,
+                        commit.new_start_line,
+                        commit.block_name,
+                        u'\n'.join(commit.diff_lines))))
     if len(rows) >= BATCH_SIZE:
-      try:
-        db.executemany(sql, rows)
-      except Exception as e:
-        print commit.repository, [commit.sha for commit in commits]
-        raise e
+      flush()
       rows = []
+  if len(rows) > 0:
+    flush()
+
+
+def commit_exists(cursor, sha):
+  """Returns whether at least one commit with a given exists in the database."""
+  cursor.execute('SELECT COUNT(*) FROM commit WHERE sha = %s', sha)
+  return cursor.fetchone()[0] > 0
+
 
 def crawl():
+  """Crawls the top repositories, collecting commits."""
   db = mysql.connect(
       host=config.DB_HOST,
       user=config.DB_USER,
       passwd=config.DB_PASSWORD,
-      db=config.DB_NAME).cursor()
+      db=config.DB_NAME)
+  cursor = db.cursor()
   gh = github.GitHub(
       config.GITHUB_CLIENT_ID,
       config.GITHUB_CLIENT_SECRET,
       config.CLIENT_USER_AGENT)
 
-  for repo in gh.GetTopRepositories():
-    print 'Starting repo ', repo.name
-    insert_repository(db, repo)
-    for commit_sha in gh.GetCommitsList(repo.name, PAGES):
-      print '  Getting SHA ' + commit_sha,
-      commits = list(gh.GetCommits(repo.name, commit_sha))
-      print '-> %d commits' % len(commits)
-      insert_commit(db, commits)
-      yield
+  try:
+    for repo in gh.GetTopRepositories():
+      print 'Starting repo ', repo.name
+      insert_repository(cursor, repo)
+      for commit_sha in gh.GetCommitsList(repo.name, PAGES):
+        if commit_exists(cursor, commit_sha):
+          print '  Skipping SHA ' + commit_sha
+        else:
+          # TODO(max99x): Log and skip errors.
+          print '  Getting SHA ' + commit_sha,
+          commits = list(gh.GetCommits(repo.name, commit_sha))
+          print '-> %d commit(s)' % len(commits)
+          insert_commit(cursor, commits)
+          yield
+  finally:
+    cursor.close()
+    db.close()
 
 
 def main():
+  """Crawls the top repositories, collecting commits."""
   crawler = crawl()
   while True:
     crawler.next()
