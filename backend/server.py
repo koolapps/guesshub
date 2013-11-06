2bf9f5fa5c8b3970df1608eee85c2544e5edd249
max99x <max99x@gmail.com>
2013-11-05 19:04:30 -0800

    Add a trivial Flasj web server for random commits.

---
+++ backend/server.py
@@ -0,0 +1,53 @@
+import config
+import flask
+import json
+import MySQLdb as mysql
+
+
+RANDOM_SQL = '''
+SELECT *
+FROM %(table)s
+  JOIN (SELECT (RAND() * (
+      SELECT MAX(order_id) FROM %(table)s)) AS value) AS random
+WHERE %(table)s.order_id >= random.value
+ORDER BY %(table)s.order_id ASC
+LIMIT %(limit)d'''
+
+
+APP = flask.Flask(__name__)
+DB = mysql.connect(
+    host=config.DB_HOST,
+    user=config.DB_USER,
+    passwd=config.DB_PASSWORD,
+    db=config.DB_NAME)
+
+
+@APP.route("/")
+def hello():
+  # TODO(max99x): Serve the client app here.
+  return "TODO(max99x): Serve the client app here."
+
+
+@APP.route("/commit")
+def commit():
+  try:
+    # TODO(max99x): Switch to dictionaries.
+    cursor = DB.cursor()
+    cursor.execute(RANDOM_SQL % {'table': 'commit', 'limit': 1})
+    commit = cursor.fetchone()
+    cursor.execute(RANDOM_SQL % {'table': 'repository', 'limit': 4})
+    repos = cursor.fetchall()
+    repos = [i for i in repos if i[2] != commit[7]][:3]
+    cursor.execute('SELECT * FROM repository WHERE name = %s', commit[7])
+    repos.append(cursor.fetchone())
+    return flask.Response(json.dumps({
+      'commit': commit,
+      'repos': repos
+    }), mimetype='text/json')
+  except Exception as e:
+    print e
+    raise e
+
+
+if __name__ == "__main__":
+  APP.run()
