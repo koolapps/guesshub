1a9e4973a100f75fa94a18637f642210b2fd1947
amasad <amjad.masad@gmail.com>
2013-11-06 13:52:23 -0500

    defaulting

--- app/client/models/user_level_progress.js
+++ app/client/models/user_level_progress.js
@@ -1,8 +1,10 @@
 var model = require('model');
+var defaults = require('model-defaults');
 
 module.exports = model('UserLevelProgress')
-  .attr('rounds')
-  .attr('completed_round')
-  .attr('guessed')
-  .attr('missed')
-  .attr('points');
+  .use(defaults)
+  .attr('rounds',             { default: 10 })
+  .attr('completed_round',    { default: 0 })
+  .attr('guessed',            { default: 0 })
+  .attr('missed',             { default: 0 })
+  .attr('points',             { default: 0 });
