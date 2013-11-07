5fc1f1e32d238868e947cfe5309271e08520ae32
max99x <max99x@gmail.com>
2013-11-06 20:16:30 -0800

    Removed DB patch (no longer relevant).

--- database_patch.sql
+++
@@ -1,8 +0,0 @@
-
-ALTER TABLE `commit_game`.`commit` 
-ADD COLUMN `order_id` INT(11) NOT NULL AUTO_INCREMENT FIRST,
-ADD UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC);
-
-ALTER TABLE `commit_game`.`repository` 
-ADD COLUMN `order_id` INT(11) NOT NULL AUTO_INCREMENT FIRST,
-ADD UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC);
