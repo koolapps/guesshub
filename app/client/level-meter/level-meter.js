7dabff69971118668e0412527fc5f1288f6209a0
amasad <amjad.masad@gmail.com>
2013-11-08 21:44:47 -0500

    --reactive

--- app/client/level-meter/level-meter.js
+++ app/client/level-meter/level-meter.js
@@ -1,31 +1,21 @@
-var reactive = require('reactive');
-var template = require('./template');
-var $        = require('jquery');
-
-// Make it work with component/models
-reactive.get(function (obj, prop) {
-  return obj.get(prop);
-});
-reactive.set(function (obj, prop, val) {
-  return obj.set(prop, val);
-});
+var $ = require('jquery');
+var Hogan = require('hogan.js')
+var template = Hogan.compile(require('./template'));
 
 // TODO: Switch away from reactive.
 // TODO: Merge guessed, missed and progress: /docs/progress-bar-mock.png.
 // TODO: Add light gray plusses for each round still in queue.
 // TODO: Use a flashy animation on each change.
+
 module.exports = function (model) {
-  var $el = $(template);
-  var view = reactive($el[0], model);
+  var $el = $('<div/>', { class: 'level-meter-container' })
+  function update () {
+    $el.html(template.render(model.toJSON()));
+    var percentComplete = model.completed_round() / model.rounds() * 100;
+    $el.find('.inner-meter').css('width',  percentComplete + '%');
+  }
+  model.on('change', update);
+  update();
 
-  view.bind('percent-complete', function (el) {
-    function update (rounds) {
-      var percentComplete = rounds / model.rounds() * 100;
-      $(el).css('width',  percentComplete + '%');
-    }
-    model.on('change completed_round', update);
-    update(model.completed_round());
-  });
-
-  return $el[0];
+  return $el;
 };
