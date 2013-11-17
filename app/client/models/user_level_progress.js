efb19648000bf7d0a89ff66d1eb16afccab4263d
max99x <max99x@gmail.com>
2013-11-17 12:13:15 -0800

    Removed defaults from UserLevelProgress.

--- app/client/models/user_level_progress.js
+++ app/client/models/user_level_progress.js
@@ -2,9 +2,9 @@ var model = require('model');
 var plugins = require('./plugins');
 
 module.exports = plugins(model('UserLevelProgress'))
-  .attr('rounds',             { default: 10 })
-  .attr('completed_round',    { default: 0 })
-  .attr('guessed',            { default: 0 })
-  .attr('missed',             { default: 0 })
-  .attr('points',             { default: 0 })
-  .attr('mistakes_left',      { default: 0 });
+  .attr('rounds')
+  .attr('completed_round')
+  .attr('guessed')
+  .attr('missed')
+  .attr('points')
+  .attr('mistakes_left');
