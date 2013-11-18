b65016dd5965fbd807de332139b8ceff5c9aaebe
max99x <max99x@gmail.com>
2013-11-17 17:51:09 -0800

    Recurse in models' toJSON() methods.

--- app/client/models/plugins.js
+++ app/client/models/plugins.js
@@ -1,14 +1,29 @@
+var $ = require('jquery');
 var clone = require('clone');
 var defaults = require('model-defaults');
 
-/** Patches Model to clone object in toJSON(). */
+/** Patches Model.toJSON() to clone the object and recurse into arrays. */
 module.exports = function (Model) {
   return Model
     .use(function (m) {
       m.prototype.toJSON = function () {
-        return clone(this.attrs);
+        return toJSON(this);
       };
     })
     .use(defaults)
     ;
+}
+
+function toJSON(val) {
+  if (val && val.toJSON && val.attrs) {
+    var result = {};
+    for (var key in val.attrs) {
+      result[key] = toJSON(val.attrs[key]);
+    }
+    return result;
+  } else if (val instanceof Array) {
+    return $.map(val, toJSON);
+  } else {
+    return val;
+  }
 }
