605161115a90250743087ab5fde109d0de2231e6
max99x <max99x@gmail.com>
2013-11-17 22:34:08 -0800

    Remove resolved TODO about locked levels.

--- app/client/level-hub/level-hub.js
+++ app/client/level-hub/level-hub.js
@@ -4,7 +4,6 @@ var template = Hogan.compile(require('./template'));
 
 // Renders the level hub for a given campaign, calling back when a level is
 // selected passing the Level object.
-// TODO: Support locked/unlocked levels.
 module.exports = function (campaign, user, callback) {
   var $el = $('<div/>', { class: 'level-hub' });
   $el.html(template.render(campaign.toJSONWithUserProgress(user)));
