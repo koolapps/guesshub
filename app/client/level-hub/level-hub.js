be5b69137c04e24622628999149a6e9ad3f51516
amasad <amjad.masad@gmail.com>
2013-11-18 01:39:53 -0500

    level template partial

--- app/client/level-hub/level-hub.js
+++ app/client/level-hub/level-hub.js
@@ -1,13 +1,18 @@
 var $ = require('jquery');
 var Hogan = require('hogan.js');
 var template = Hogan.compile(require('./template'));
+var levelTemplate = Hogan.compile(require('./level-template'));
 
 // Renders the level hub for a given campaign, calling back when a level is
 // selected passing the Level object.
 // TODO: Support locked/unlocked levels.
 module.exports = function (campaign, user, callback) {
   var $el = $('<div/>', { class: 'level-hub' });
-  $el.html(template.render(campaign.toJSONWithUserProgress(user)));
+  $el.html(
+    template.render(
+      campaign.toJSONWithUserProgress(user), { levelTemplate: levelTemplate }
+    )
+  );
 
   $el.on('click', '.level', function () {
     var id = parseInt($(this).data('id'), 10);
