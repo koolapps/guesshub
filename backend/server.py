470e3e1f863f500b9a991b196afa7f12b6783671
max99x <max99x@gmail.com>
2013-11-21 00:07:29 -0800

    Keep a commit grade index in memory.

--- backend/server.py
+++ backend/server.py
@@ -2,46 +2,49 @@ import collections
 import config
 import flask
 import json
+import bisect
 import random
 import MySQLdb as mysql
 import MySQLdb.cursors
 
-
-RANDOM_COMMIT_SQL = '''
-SELECT *
-FROM commit USE INDEX (grade_order_index)
-  JOIN (SELECT (RAND() * (
-      SELECT MAX(order_id) FROM commit)) AS value) AS random
-WHERE commit.order_id %s random.value
-  AND commit.grade > %d AND commit.grade <= %d
-ORDER BY commit.order_id ASC
-LIMIT 1'''
-
 APP = flask.Flask(__name__, static_folder='../app', static_url_path='')
-# TODO: Figure out when the connection should be reinitialized.
-DB = mysql.connect(
-    host=config.DB_HOST,
-    user=config.DB_USER,
-    passwd=config.DB_PASSWORD,
-    db=config.DB_NAME,
-    cursorclass=MySQLdb.cursors.DictCursor)
 
+def connect_to_db():
+  return mysql.connect(
+      host=config.DB_HOST,
+      user=config.DB_USER,
+      passwd=config.DB_PASSWORD,
+      db=config.DB_NAME,
+      cursorclass=MySQLdb.cursors.DictCursor).cursor()
 
-def load_repos():
-  cursor = DB.cursor()
+def initialize():
+  cursor = connect_to_db()
+
+  # Get all repositories.
   cursor.execute('SELECT * FROM repository')
   first_row = cursor.fetchone()
   repo_class = collections.namedtuple('Repository', ' '.join(first_row.keys()))
-  result = {}
-  result[first_row['name']] = repo_class(**first_row)
+  repos = {}
+  repos[first_row['name']] = repo_class(**first_row)
   for row in cursor.fetchall():
-    result[row['name']] = repo_class(**row)
+    repos[row['name']] = repo_class(**row)
 
-  return result
+  # Get a commit index.
+  cursor.execute('SELECT grade, order_id '
+                 'FROM commit WHERE grade >= 0 '
+                 'ORDER BY GRADE')
+  grades = []
+  ids = []
+  for commit in cursor.fetchall():
+    grades.append(commit['grade'])
+    ids.append(commit['order_id'])
+  index_class = collections.namedtuple('Index', 'grades ids')
+  commit_index = index_class(tuple(grades), tuple(ids))
 
+  return repos, commit_index
 
-# Assume we have enough memory to keep all repositories loaded.
-REPOS = load_repos()
+# Assume we have enough memory to keep all repositories and a commit index loaded.
+REPOS, COMMIT_INDEX = initialize()
 REPO_NAMES = REPOS.keys()
 
 
@@ -49,34 +52,31 @@ REPO_NAMES = REPOS.keys()
 def homepage():
   return open('../app/index.html', 'r').read();
 
-def get_round(grade_lower_bound, grade_upper_bound):
-  cursor = DB.cursor()
-  if not cursor.execute(RANDOM_COMMIT_SQL % ('>=', grade_lower_bound, grade_upper_bound)):
-    cursor.execute(RANDOM_COMMIT_SQL % ('<', grade_lower_bound, grade_upper_bound))
-  commit = cursor.fetchone()
-  repo_names = random.sample(REPO_NAMES, 4)
-  if commit['repository'] not in repo_names:
-    repo_names = repo_names[:3] + [commit['repository']]
-  repos = [REPOS[i]._asdict() for i in repo_names]
-  random.shuffle(repos)
-  return {
-    'commit': commit,
-    'repos': repos
-  }
 
-@APP.route("/level/<length>/<grade_lower_bound>/<grade_upper_bound>")
-def level(length, grade_lower_bound, grade_upper_bound):
+@APP.route("/level/<length>/<min_grade>/<max_grade>")
+def level(length, min_grade, max_grade):
   length = int(length)
-  grade_upper_bound = int(grade_upper_bound)
-  grade_lower_bound = int(grade_lower_bound)
+  max_grade = int(max_grade)
+  min_grade = int(min_grade)
 
-  level_rounds = []
-  for i in range(0, length):
-    level_rounds.append(get_round(grade_lower_bound, grade_upper_bound))
+  start = bisect.bisect_left(COMMIT_INDEX.grades, min_grade)
+  end = bisect.bisect_right(COMMIT_INDEX.grades, max_grade)
+  ids = [COMMIT_INDEX.ids[i] for i in random.sample(xrange(start, end), length)]
 
-  return flask.Response(json.dumps({
-    'rounds': level_rounds
-  }), mimetype='text/json')
+  sql = 'SELECT * FROM commit WHERE order_id IN (%s)' % ','.join(map(str, ids))
+  cursor = connect_to_db()
+  cursor.execute(sql)
+
+  levels = []
+  for commit in cursor.fetchall():
+    repo_names = random.sample(REPO_NAMES, 4)
+    if commit['repository'] not in repo_names:
+      repo_names = repo_names[:3] + [commit['repository']]
+    repos = [REPOS[i]._asdict() for i in repo_names]
+    random.shuffle(repos)
+    levels.append({'commit': commit, 'repos': repos})
+
+  return flask.Response(json.dumps({'rounds': levels}), mimetype='text/json')
 
 
 if __name__ == "__main__":
