05c95fd9ab1938f08e5b026d705825a913894e5c
max99x <max99x@gmail.com>
2013-11-22 01:37:41 -0800

    Add an audio module.

---
+++ app/client/audio/audio.js
@@ -0,0 +1,51 @@
+var AudioPlayer = {};
+
+var FILES = {
+  // TODO: Add effects.
+};
+
+var context;
+var buffers;
+var initialized = false;
+
+AudioPlayer.initialize = function () {
+  if (!initialized) {
+    context = new (window.AudioContext || window.webkitAudioContext)();
+    buffers = {};
+    for (var effectName in FILES) {
+      AudioPlayer.loadSound(effectName);
+    }
+    initialized = true;
+  }
+};
+
+AudioPlayer.loadSound = function (effectName) {
+  var filename = FILES[effectName];
+  if (filename) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', filename, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(e) {
+      if (xhr.status == 200) {
+        context.decodeAudioData(xhr.response, function(buffer) {
+          buffers[effectName] = buffer;
+        });
+      }
+    };
+    xhr.send();
+  }
+};
+
+AudioPlayer.play = function (effectName) {
+  var buffer = buffers[effectName];
+  if (buffer) {
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
+  }
+};
+
+AudioPlayer.initialize();
+
+module.exports = AudioPlayer;
