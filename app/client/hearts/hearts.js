a1170983e697e05cfbed84f53492560cd1fb11e1
amasad <amjad.masad@gmail.com>
2013-11-23 23:27:25 -0500

    Hearts animation + use amasad/animation

--- app/client/hearts/hearts.js
+++ app/client/hearts/hearts.js
@@ -1,13 +1,27 @@
 var $ = require('jquery');
 var template = require('./template');
+var animate = require('animate');
 
 module.exports = function (levelProgress) {
   var $el = $('<div/>', { class: 'hearts' });
 
   function update (mistakesLeft) {
-    $el.empty();
-    while (mistakesLeft-- > 0) {
-      $el.append(template);
+    if (mistakesLeft < 0) {
+      return;
+    }
+    var hearts = $el.find('.fa-heart').toArray();
+    if (hearts.length < mistakesLeft) {
+      while (mistakesLeft-- > 0) {
+        $el.append(template);
+      }
+    } else {
+      while (hearts.length > mistakesLeft) {
+        var heart = hearts.pop();
+
+        animate.out(heart, 'rotate-up-right', false, function () {
+          $(heart).remove();
+        });
+      }
     }
   }
 
