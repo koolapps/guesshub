89e7f6d7e600365c909a67baccf90f23791aefd4
max99x <max99x@gmail.com>
2013-11-25 10:22:55 -0800

    Remove (disabled) GitHub API key.

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,6 @@
-# TODO(max99x): Disable the account with the checked-in credentials.
-
 # GitHub auth.
 GITHUB_CLIENT_ID = '6e91d029d2eeca74bf24'
-GITHUB_CLIENT_SECRET = '70129cd2a9d2b993a19b17a4c38696b96c50d725'
+GITHUB_CLIENT_SECRET = ''
 
 # The user agent to use for GitHub requests.
 CLIENT_USER_AGENT = 'max99x/game-off-2013'
