c7102bd3aa1b8e81d7571279bfde7442b821503f
max99x <max99x@gmail.com>
2013-11-28 17:29:24 -0800

    Add a script to limit the number of enabled commits per repository.

---
+++ backend/equalize.py
@@ -0,0 +1,37 @@
+import config
+import collections
+import math
+import json
+import re
+import sys
+import MySQLdb as mysql
+import MySQLdb.cursors as mysql_cursors
+from os import path
+
+MAX_ROWS = 20000
+
+def main():
+  db = mysql.connect(
+      host=config.DB_HOST,
+      user=config.DB_USER,
+      passwd=config.DB_PASSWORD,
+      db=config.DB_NAME)
+  cursor = db.cursor()
+  cursor.execute('SELECT repository, COUNT(*) as count '
+                 'FROM commit '
+                 'WHERE grade >= 0 '
+                 'GROUP BY repository '
+                 'HAVING count >= %s '
+                 'ORDER BY count DESC',
+                 (MAX_ROWS,))
+
+  for repo, count in cursor.fetchall():
+    cursor.execute('UPDATE commit '
+                   'SET grade=-grade '
+                   'WHERE grade >= 0 AND repository = %s '
+                   'LIMIT %s',
+                   (repo, count - MAX_ROWS))
+
+
+if __name__ == '__main__':
+  main()
