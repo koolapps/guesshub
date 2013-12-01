71d15be7229cb2668815518053230b8b96d319d5
Amjad Masad <amjad.masad@gmail.com>
2013-12-01 04:29:07 -0500

    protect against accidental history back clicks using a prompt

--- app/lib/boot/router.js
+++ app/lib/boot/router.js
@@ -7,6 +7,8 @@ var location = window.location;
 
 var pushStateSupported = 'pushState' in window.history;
 
+var prevPath = location.pathname;
+
 exports.start = function (game) {
   if (!pushStateSupported) {
     return;
@@ -24,7 +26,19 @@ exports.start = function (game) {
   $(window).on('popstate', function (e) {
     // Make sure it's not initial chrome popstate bug.
     if (e.originalEvent.state) {
-      router.dispatch(location.pathname);
+      var dispatch = true;
+      if (prevPath.match(/^\/level/)) {
+        dispatch = confirm(
+          'Are you sure you want to navigate away?\n' +
+          'Level progress will be lost.'
+        );
+      }
+      if (dispatch) {
+        prevPath = location.pathname;
+        router.dispatch(location.pathname);
+      } else {
+        exports.navigate(prevPath);
+      }
     }
   });
 
@@ -34,6 +48,7 @@ exports.start = function (game) {
 
 exports.navigate = function (path) {
   if (pushStateSupported && path !== location.pathname) {
+    prevPath = path;
     window.history.pushState({}, null, path);
   }
 };
